fea99f5ca30729f442a8d1d5cbc5e3c67780cde9
Gudvin <gudvin@nextbi.ru>
2023-04-27 16:57:53 +0300

    + 5.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
 
+  static primeClass = "notification";
+
+  constructor(message = "", {duration = 0, type = ""} = {}) {
+    this.type = type;
+    this.duration = duration;
+    this.message = message;
+    this.classname = `${NotificationMessage.primeClass} ${this.type}`;
+    this.element = this.render();
+  }
+
+  show() {
+/*    let elements = document.getElementsByClassName(NotificationMessage.primeClass);
+    if (elements.length === 0) {
+      document.body.append(this.element);
+    }*/
+    setTimeout(this.remove, this.duration);
+  }
+
+  render() {
+    let element = document.createElement('div');
+    element.className = this.classname;
+    element.style.cssText = `--value:${this.duration / 1000}s`;
+    element.innerHTML = `
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>`;
+    /*this.element = element;*/
+    let elements = document.getElementsByClassName(NotificationMessage.primeClass);
+    if (elements.length === 0) {
+      document.body.append(this.element);
+    }
+    return element;
+  }
+
+  update() {}
+
+  destroy() {
+    /*this.remove();*/
+    this.element = null;
+  }
+
+  remove() {
+    let elements = document.getElementsByClassName(NotificationMessage.primeClass);
+    if (this.element != null) {
+      this.element.remove();
+    }
+    if (elements.length > 0) {
+      for (let element of elements) {
+        element.remove();
+      }
+    }
+  }
+
 }
