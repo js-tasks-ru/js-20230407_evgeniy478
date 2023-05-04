f087e000a471d7c565f8fc6a6c4794b169142215
Gudvin <gudvin@nextbi.ru>
2023-05-04 22:31:26 +0300

    + 6.2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,64 @@
 class Tooltip {
-  initialize () {
 
+  static instance;
+
+  constructor() {
+    return Tooltip.instance;
+  }
+
+  initialize () {
+    if (!Tooltip.instance) {
+      Tooltip.instance = this;
+    }
+    this.addListeners();
+    this.pointerSelector = "tooltip";
+    return Tooltip.instance;
+  }
+
+  render (toolTip = "") {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `<div class="${this.pointerSelector}">${toolTip}</div>`;
+    this.element = wrapper.firstChild;
+    document.body.append(this.element);
+  }
+
+  mouseOver = event => {
+    this.element.style.left = event.clientX + 15 + "px";
+    this.element.style.top = event.clientY + 15 + "px";
+  }
+
+  mouseOut = event => {
+    const element = event.target.closest("div");
+
+    if (element) {
+      this.render(element.dataset.tooltip);
+      document.body.addEventListener("pointermove", this.mouseOver);
+    }
+  }
+
+  removeLabel = () => {
+    this.destroy();
+    document.body.removeEventListener("pointermove", this.mouseOver);
+  }
+
+  addListeners () {
+    document.body.addEventListener("pointerover", this.mouseOut);
+    document.body.addEventListener("pointerout", this.removeLabel);
+  }
+
+  destroy () {
+    if (this.element) {
+      this.remove();
+    }
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
   }
 }
+
+
 
 export default Tooltip;
