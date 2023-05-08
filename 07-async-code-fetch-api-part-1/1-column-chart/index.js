182948d31163a923acf54bb98dca57ac62f3fe37
Gudvin <gudvin@nextbi.ru>
2023-05-08 12:37:45 +0300

    отработка замечаний по 7.1

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,8 +1,13 @@
+import fetchJson from './utils/fetch-json.js';
+
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ColumnChart {
 
-  constructor({url = "", range = {from: "2023-04-05", to: "2023-05-05"}, label = "", link = "#"} = {}) {
+  static chartHeight = 50;
+
+  constructor({url = "", range = {from: "2023-04-05", to: "2023-05-05"},
+    label = "", link = "#", formatHeading = data => `$${data}`} = {}) {
     this.url = url;
     this.range = range;
     this.label = label;
@@ -10,13 +15,20 @@ export default class ColumnChart {
     this.data = [];
     this.from = new Date(Date.parse(range.from));
     this.to = new Date(Date.parse(range.to));
+    this.format = formatHeading;
     this.render();
-    setTimeout(this.simulateClick, "1000");
+    //setTimeout(this.simulateClick, "1000");
   }
 
   render() {
     const wrapper = document.createElement("div");
-    wrapper.innerHTML = `<div class="column-chart column-chart_loading" style="--chart-height: 50">
+    wrapper.innerHTML = this.template();
+    this.element = wrapper.firstElementChild;
+    this.update(this.from, this.to);
+  }
+
+  template() {
+    return `<div class="column-chart column-chart_loading" style="--chart-height: ${ColumnChart.chartHeight}">
       <div class="column-chart__title">
         Total ${this.label}
         <a class="column-chart__link" href="#">${this.label === "orders" ? "View all" : ""}</a>
@@ -30,21 +42,10 @@ export default class ColumnChart {
         </div>
       </div>
     </div>`;
-    this.element = wrapper.firstElementChild;
-    this.update(this.from, this.to);
-  }
-
-  formatDollars(number = 0) {
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      maximumFractionDigits: 0
-    });
-    return formatter.format(number);
   }
 
   update (from = new Date(), to = new Date()) {
-    for (let elementsByClassNameElement of document.getElementsByClassName("column-chart")) {
+    for (const elementsByClassNameElement of document.getElementsByClassName("column-chart")) {
       elementsByClassNameElement.classList.remove("column-chart_loading");
       this
         .loadValues(BACKEND_URL + "/" + this.url.replace("orders", elementsByClassNameElement.parentNode.id)
@@ -54,18 +55,18 @@ export default class ColumnChart {
   }
 
   showValues (data, elementsByClassNameElement) {
-    let max = Object.values(data).reduce(
+    const max = Object.values(data).reduce(
       (accumulator, currentValue) => accumulator > currentValue ? accumulator : currentValue);
-    let sum = Object.values(data).reduce((accumulator, currentValue) => accumulator + currentValue);
-    let string = Object.values(data).map(value => {
-      let percent = (value / max) * 100;
-      return `<div style="--value: ${value * (50 / max)}" data-tooltip="${percent + "%"}"></div>`;
+    const sum = Object.values(data).reduce((accumulator, currentValue) => accumulator + currentValue);
+    const string = Object.values(data).map(value => {
+      const percent = (value / max) * 100;
+      return `<div style="--value: ${value * (ColumnChart.chartHeight / max)}" data-tooltip="${percent + "%"}"></div>`;
     }).join("");
     elementsByClassNameElement.innerHTML = `<div class="column-chart__title">
         Total ${elementsByClassNameElement.parentNode.id}
       </div>
       <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">${elementsByClassNameElement.parentNode.id === "sales" ? this.formatDollars(sum) : sum}</div>
+        <div data-element="header" class="column-chart__header">${elementsByClassNameElement.parentNode.id === "sales" ? this.format(sum) : sum}</div>
         <div data-element="body" class="column-chart__chart">
           ${string}
         </div>
@@ -73,17 +74,7 @@ export default class ColumnChart {
   }
 
   loadValues(url) {
-    return fetch(url).then(response => response.json());
-  }
-
-  simulateClick() {
-    const event = new MouseEvent("click", {
-      view: window,
-      bubbles: true,
-      cancelable: true,
-    });
-    const cb = document.getElementById("loadOrders");
-    cb.dispatchEvent(event);
+    return fetchJson(url);
   }
 
   destroy () {
